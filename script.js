ccd7795b1428300ade74b76bb62914190cbd54a3
ShivamP-97 <shivampathak19972@gmail.com>
2025-10-17 23:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+ document.addEventListener('DOMContentLoaded', function () {
+   const input = document.getElementById('evaluatedText');
+   const wordCount = document.getElementById('wordCount');
+
+ input.addEventListener('input', function () {
+    const text = input.value.trim();
+    const words = text === "" ? [] : text.split(/\s+/);
+    wordCount.textContent = words.length;
+  });
+});
